88889399c9ae6b0bd8d888ee52222ff7dfd257f6
believehan <90814456+believehan@users.noreply.github.com>
2025-09-02 11:21:28 +0900

    opencv이용해 이미지 증강

---
+++ v06_data_aug/v06_2_cv2.py
@@ -0,0 +1,34 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# 1. 이미지 로드
+img = cv2.imread("v06_data_aug/data_aug.jpg")
+
+# 2. 이미지 조정(수평 반전)
+img_flipped = cv2.flip(img, 1) # 1은 수평, 0은 수직, -1은 둘 다
+img_flipped2 = cv2.flip(img, 0) # 1은 수평, 0은 수직, -1은 둘 다
+img_flipped3 = cv2.flip(img, -1) # 1은 수평, 0은 수직, -1은 둘 다
+
+# 3. 이미지 시각화
+fig, ax = plt.subplots(2,2, figsize=(10, 5))
+ax[0,0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+ax[0,0].axis('off')
+ax[0,0].set_title("Original")
+
+ax[0,1].imshow(img_flipped)
+ax[0,1].axis('off')
+ax[0,1].set_title("Flipped")
+
+ax[1,0].imshow(img_flipped2)
+ax[1,0].axis('off')
+ax[1,0].set_title("Flipped2")
+
+ax[1,1].imshow(img_flipped3)
+ax[1,1].axis('off')
+ax[1,1].set_title("Flipped3")
+plt.show()
+######################### 시각화 (2,2) 이미지 채우고  RGB로 변환
+
+# 4. 이미지 저장
+cv2.imwrite("./img_flipped.jpg", img_flipped)
+print("이미지 저장 완료")
